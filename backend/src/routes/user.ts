0cb6a9c889df330853e69e9870d7f38e03f5dcd2
Wilhelmine <mine.erdbeer@gmail.com>
2024-08-06 13:34:22 +0200

    implement jwt in registration

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,10 +1,50 @@
 import express, { Request, Response, NextFunction } from 'express'
 import {User} from '../models/User'
+import { createHash } from 'crypto'
+import validator from 'validator'
 
 const app = express.Router()
 
+// -> /user
 app.route('/')
-    .get()
-    .post()
+    .get(async (req:Request, res:Response, next:NextFunction)=>{
+        try {
+           const result = await User.find()
+           res.json(result)
+        } catch (error) {
+            console.error('Kein User gefunden')
+            next(error)
+        }
+    })
+    .post(async (req:Request, res:Response, next:NextFunction)=>{
+        try {
+            const sha256 = createHash('sha256')
+            const newUser = req.body
+
+            // Passwort validieren
+            const isStrong = validator.isStrongPassword(newUser.password, {
+                minNumbers: 2,
+                minUppercase: 2,
+                minSymbols: 2
+            })
+            if(!isStrong){
+                throw new Error('weak password')
+            }
+
+            // Passwort hashen
+            newUser.password = sha256.copy().update(newUser.password).digest('hex')
+
+            // User in DB speichern
+            await User.create(newUser)
+
+            res.sendStatus(201)
+
+
+        } catch (error) {
+            next(error)
+        }
+    })
 
 export default app
+
+// 12345678ABab$$
